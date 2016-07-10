542fe91bded5bcd3d53b11a06eff973187438fc7
DongDong <429507692@qq.com>
2016-07-10 21:15:58 +0800

    MD
    Signed-off-by: DongDong <429507692@qq.com>

---
+++ DongStudy/app/src/main/java/com/dong/study/support/ImmerseActivity.java
@@ -0,0 +1,72 @@
+package com.dong.study.support;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+import com.dong.study.R;
+
+/**
+ * Created by Zdd on 2016/7/10.
+ */
+public class ImmerseActivity extends AppCompatActivity {
+
+    /**
+     * 兼容
+     * 5.0(v21)
+     * // 主题
+     * colorPrimary      操作栏(toolbar等)
+     * colorPrimaryDark  顶部状态栏
+     * colorAccent       MD 主色调
+     * <p>
+     * // 样式
+     * android:navigationBarColor 底部导航栏
+     * ...
+     * <p>
+     * //代码
+     * getWindow().setNavigationBarColor();
+     * getWindow().setStatusBarColor();
+     * <p>
+     * <p>
+     * 4.4(v19)
+     * 通过设置状态栏透明实现效果
+     * // 主题 (不推荐)
+     * android:windowTranslucentNavigation
+     * android:windowTranslucentStatus
+     * <p>
+     * // 代码
+     * 在 setContentView 前加入
+     * getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+     * <p>
+     * -- 副作用:内容会占据 status 栏
+     * 1. 解决:给顶部view(如toolbar)设置 android:fitsSystemWindows="true"
+     * 让view根据系统窗口(包含状态栏等)来调整自己的布局, true:调用整个view的padding来给系统窗口留出空间
+     * <p>
+     * -- 但是如果里面有ScrollView+EditText 打开键盘时 会把toolbar拉下来
+     * 1.1 解决:给最外层设置 android:fitsSystemWindows="true" 在设置背景色, 内部 ScrollView 设置另一套背景色
+     * 1.2 解决: 给 Toolbar 设置 fitsSystemWindows 并修改 Toolbar 高度 PaddingTop: toolbarHeight + getStatusBarHeight
+     */
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_immerse);
+
+    }
+
+    private int getStatusBarHeight(Context context) {
+        try {
+            Class clazz = Class.forName("com.android.internal.R&dimen");
+            Object object = clazz.newInstance();
+            String heightStr = clazz.getField("status_bar_height").get(object).toString();
+            // 获取的是 id
+            int heightId = Integer.parseInt(heightStr);
+            // dp -> px
+            return context.getResources().getDimensionPixelSize(heightId);
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+}
